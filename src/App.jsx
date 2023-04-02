a45d567dc5084e0041c4c8c0923701e6fb88962a
skdan <skdanish1999@gmail.com>
2023-04-02 11:00:32 +0530

    install vite and setup chakraui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <Box className="App">
+      <Heading>Hello world</Heading>
+      <Button variant="outline">Count</Button>
+    </Box>
+  );
+}
+
+export default App;
