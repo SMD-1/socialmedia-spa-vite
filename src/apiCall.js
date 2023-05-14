46f3632ae2c8b2f6ca0bde0ef0b6268cb72baef8
skdan <skdanish1999@gmail.com>
2023-05-14 16:36:10 +0530

    make api call for register and completesignup

--- src/apiCall.js
+++ src/apiCall.js
@@ -2,6 +2,60 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { prefix } from "./apiconfig";
 
+export const registerCall = async (userCredential, dispatch) => {
+  dispatch({ type: "REGISTER_START" });
+  try {
+    const res = await axios.post(prefix + "auth/register", userCredential);
+    dispatch({ type: "REGISTER_SUCCESS", payload: res.data });
+    if (res.status === 200) {
+      toast("Registered Successfully!", {
+        position: "top-right",
+        type: "success",
+        theme: "dark",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+    }
+  } catch (err) {
+    // console.log(err.message);
+    toast(err.response.data.msg, {
+      position: "top-right",
+      type: "error",
+      theme: "dark",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+    dispatch({ type: "REGISTER_FAILURE", payload: err });
+  }
+};
+
+export const updateUser = async (data, dispatch) => {
+  const id = data.userId;
+  try {
+    const res = await axios.patch(prefix + `users/${id}`, data);
+    // console.log("success update", res.data);
+    dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+  } catch (err) {
+    toast(err.response.data.msg, {
+      position: "top-right",
+      type: "error",
+      theme: "dark",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+    dispatch({ type: "UPDATE_FAILURE", payload: err.response.data.msg });
+  }
+};
+
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
@@ -20,14 +74,6 @@ export const loginCall = async (userCredential, dispatch) => {
       });
     }
   } catch (err) {
-    // toast({
-    //   position: "top-right",
-    //   title: "Error.",
-    //   description: err.response.data.msg,
-    //   status: "error",
-    //   duration: 3000,
-    //   isClosable: true,
-    // });
     toast(err.response.data.msg, {
       position: "top-right",
       type: "error",
@@ -38,7 +84,7 @@ export const loginCall = async (userCredential, dispatch) => {
       pauseOnHover: true,
       draggable: true,
     });
-    dispatch({ type: "LOGIN_FAILURE", payload: err });
-    console.log(err.response);
+    dispatch({ type: "LOGIN_FAILURE", payload: err.response.data.msg });
+    // console.log(err.response.data.msg);
   }
 };
